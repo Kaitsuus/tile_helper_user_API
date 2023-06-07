ae8d02269e6464bf530d765a80b868f184f0a5ff
Kaitsuus <kai.jukarainen@gmail.com>
2023-06-07 11:58:20 +0300

    add: UNIT-test List

--- tests/list_API.test.js
+++ tests/list_API.test.js
@@ -59,6 +59,81 @@ describe('LIST CREATION:', () => {
     const listsAtEnd = await testHelper.listsInDb();
     expect(listsAtEnd).toHaveLength(1);
   });
+
+  test('all lists are returned', async () => {
+    const userId = testMaterials.users[0]._id;
+
+    // Create two lists for the user
+    for (const list of testMaterials.lists) {
+      await api
+        .post(`/api/users/${userId}/lists`)
+        .set('Authorization', `bearer ${testToken}`)
+        .send(list);
+    }
+
+    const response = await api
+      .get(`/api/users/${userId}/lists`)
+      .set('Authorization', `bearer ${testToken}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toHaveLength(2);
+  });
+});
+describe('LIST DELETION:', () => {
+  test('a list can be deleted with a valid token', async () => {
+    // Create a list
+    const newList = testMaterials.lists[0];
+    const userId = testMaterials.users[0]._id;
+
+    const createdList = await api
+      .post(`/api/users/${userId}/lists`)
+      .set('Authorization', `bearer ${testToken}`)
+      .send(newList)
+      .expect(201);
+    // console.log('Created list:', createdList.body);
+
+    // Delete the list
+    const deleteEndpoint = `/api/users/${userId}/lists/${createdList.body.id}`;
+    // console.log('Delete endpoint:', deleteEndpoint);
+    // console.log('Test token:', testToken);
+    // console.log(`ListId: ${createdList.body.id}`);
+
+    await api
+      .delete(deleteEndpoint)
+      .set('Authorization', `bearer ${testToken}`)
+      .expect(204);
+
+    // Check if the list is deleted
+    const listsAtEnd = await testHelper.listsInDb();
+    expect(listsAtEnd).toHaveLength(0);
+  });
+});
+describe('LIST UPDATING:', () => {
+  test('a list can be updated', async () => {
+    // Create a list for the user
+    const newList = testMaterials.lists[0];
+    const userId = testMaterials.users[0]._id;
+
+    const createdList = await api
+      .post(`/api/users/${userId}/lists`)
+      .set('Authorization', `bearer ${testToken}`)
+      .send(newList)
+      .expect(201);
+
+    const updateEndpoint = `/api/users/${userId}/lists/${createdList.body.id}`;
+
+    const updatedList = { ...newList, title: 'Updated List' };
+
+    const response = await api
+      .put(updateEndpoint)
+      .set('Authorization', `bearer ${testToken}`)
+      .send(updatedList)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.name).toEqual(updatedList.name);
+  });
 });
 
 afterAll(async () => {
